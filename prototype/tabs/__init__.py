3b4d53da3a39e3b7b7acc65cd34fb5c60aedc327
Hansy21 <han8322652@gmail.com>
2026-08-06 20:02:40 +0800

    Add Data Analysis tab and update app layout; configure EDA directory

--- prototype/tabs/__init__.py
+++ prototype/tabs/__init__.py
@@ -1,6 +1,7 @@
 """Main-page tabs for the Streamlit prototype."""
 
+from prototype.tabs.analysis import render_analysis_tab
 from prototype.tabs.insights import render_insights_tab
 from prototype.tabs.predict import render_predict_tab
 
-__all__ = ["render_predict_tab", "render_insights_tab"]
+__all__ = ["render_predict_tab", "render_insights_tab", "render_analysis_tab"]
